2bd784d41bb6842ade074d285e808f5afe1660d1
Reiji <reiji.tokuda@up-sider.com>
2024-05-07 08:21:51 +0900

    Initial commit

---
+++ apis/api-a/src/index.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+//import * as core from 'express-serve-static-core';
+//import HttpStatusCodes from '@app/constants/HttpStatusCodes';
+
+const router = express.Router()
+
+export interface IApiA {
+  message: string;
+}
+
+export interface IApiAReq<T = void> extends express.Request {
+  body: T;
+}
+
+export interface IApiARes extends express.Response {
+  locals: Record<string, unknown>;
+}
+
+/**
+ * 
+ */
+async function ApiA(req: IApiAReq<{ a: IApiA}>, res: IApiARes) {
+  const { a } = req.body;
+  console.log(a.message);
+  //console.log(req);
+  //return res.status(200).send("api-a" + a.message);
+  //return res.send("api-a" + b.message);
+  return res;
+}
+
+router.get('/', ApiA);
+
+export default router;
---
+++ apis/api-b/src/index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+
+const router = express.Router()
+
+export interface IApiB {
+  message: string;
+}
+
+export interface IApiBReq<T = void> extends express.Request {
+  body: T;
+}
+
+export interface IApiBRes extends express.Response {
+  locals: Record<string, unknown>;
+}
+
+/**
+ * 
+ */
+async function ApiB(req: IApiBReq<{ b: IApiB}>, res: IApiBRes) {
+  const { b } = req.body;
+  console.log(b.message);
+  return res;
+}
+
+router.get('/', ApiB);
+
+export default router;
